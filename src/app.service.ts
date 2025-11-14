8f5578783dfcf52e9b9692b4e8136b6211577d2a
Fernando Mero <sd.kettei@gmail.com>
2025-11-14 12:42:36 -0500

    Mejorado Saludo

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,25 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'App Findex';
+  getHello(): object {
+    return {
+      app_name: 'App Findex',
+      app_endpoints: [
+        '/prestamista',
+        '/direccion',
+        '/frecuencia-pago',
+        '/prestatario',
+        '/metodo-pago',
+        '/rango',
+        '/ciudad',
+        '/barrio',
+        '/pago',
+        '/solicitud-prestamo',
+        '/oferta-prestamo',
+        '/usuarios',
+        '/prestamo',
+        '/tipo-usuario',
+      ]
+    };
   }
 }
